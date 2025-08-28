83c520e579efaac537e82fe8c8cd89f547ad97d2
ShanLeiGuang <13903118812@139.com>
2025-08-28 11:16:51 +0800

    更新背景图生成脚本

--- canvas/canvas.pl
+++ canvas/canvas.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/perl
-#by shanleiguang, 2024.1.5
+#canvas是vRain兀雨古籍刻本电子书制作工具的背景制作脚本
+#by shanleiguang@gmail.com, 2024/1, 2025/8
 use strict;
 use warnings;
 
@@ -7,6 +8,8 @@ use Image::Magick;
 use Getopt::Std;
 use Encode;
 use utf8;
+
+$| = 1; #autoflush
 
 binmode(STDIN, ':encoding(utf8)');
 binmode(STDOUT, ':encoding(utf8)');
@@ -36,6 +39,9 @@ while(<CONFIG>) {
 }
 close(CONFIG);
 
+my ($ifmr, $mrn, $mcc) = ($canvas{'if_multirows'}, $canvas{'multirows_num'}, $canvas{'multirows_colcolor'});
+
+my $bg = $canvas{'canvas_background_image'}; #背景图
 my ($cw, $ch, $cc) = ($canvas{'canvas_width'}, $canvas{'canvas_height'}, $canvas{'canvas_color'});
 my ($mt, $mb) = ($canvas{'margins_top'}, $canvas{'margins_bottom'});
 my ($ml, $mr) = ($canvas{'margins_left'}, $canvas{'margins_right'});
@@ -51,35 +57,69 @@ my ($flw, $flm, $flc) = ($canvas{'fish_line_width'}, $canvas{'fish_line_margin'}
 my ($ilw, $ilc) = ($canvas{'inline_width'}, $canvas{'inline_color'});
 my ($olw, $olc) = ($canvas{'outline_width'}, $canvas{'outline_color'});
 my ($moh, $mov) = ($canvas{'outline_hmargin'}, $canvas{'outline_vmargin'});
-my ($lgt, $lgy, $lgc) = ($canvas{'logo_text'}, $canvas{'logo_y'}, $canvas{'logo_color'});
+my ($lgi, $lgt, $lgy, $lgc) = ($canvas{'logo_image'}, $canvas{'logo_text'}, $canvas{'logo_y'}, $canvas{'logo_color'});
 my ($lgf, $lgs) = ($canvas{'logo_font'}, $canvas{'logo_font_size'});
-my $clw = ($cw-$ml-$mr-$lcw)/$cln;
+my $clw = ($cw-$ml-$mr-$lcw)/$cln; #计算列宽
 
-$cc = 'white' if(not $cc);
+$cc = 'white' if(not $cc); #背景色未定义时采用白色
 print "create ImageMagick ...\n";
 my $cimg = Image::Magick->new();
 
 $cimg->Set(size => $cw.'x'.$ch);
-$cimg->ReadImage("canvas:$cc");
 
-$cimg->Draw(primitive => 'rectangle', points => get_points($ml-$olw/2-$moh, $mt-$olw/2-$mov, $cw-$mr+$olw/2+$moh, $ch-$mb+$olw/2+$mov),
-	fill => 'transparent', stroke => $olc, strokewidth => $olw);
-$cimg->Draw(primitive => 'rectangle', points => get_points($ml, $mt, $cw-$mr, $ch-$mb),
-	fill => 'transparent', stroke => $ilc, strokewidth => $ilw);
-
-foreach my $cid (1..$cln) {
-	my $wd = ($cid > $cln/2) ? ($lcw-$clw) : 0;
-	$cimg->Draw(primitive => 'line', points => get_points($ml+$wd+$clw*$cid, $mt, $ml+$wd+$clw*$cid, $ch-$mb),
-		stroke => $ilc, strokewidth => $ilw);
+if(defined $bg and -f $bg) {
+	$cimg->ReadImage($bg); #使用背景图
+	my ($bw, $bh) = ($cimg->Get('width'), $cimg->Get('height'));
+    if($bw != $cw or $bh != $ch) {
+        $cimg->AdaptiveResize(width => $cw, height => $ch, x => 0, y => 0, method => 'Hermit');
+    }
+} else {
+	$cimg->ReadImage("canvas:$cc");
 }
 
-draw_fishtop($fty, $ftrh, $ftth);
-if($fbd == 0) { draw_fishbtm_down($fby, $fbrh, $fbth); }
-if($fbd == 1) { draw_fishbtm_up($fby, $fbrh, $fbth); }
+my $delta = 5; #标准间距
+#粗外框
+$cimg->Draw(primitive => 'rectangle', points => get_points($ml-$olw/2-$moh-$delta, $mt-$olw/2-$mov-$delta, $cw-$mr+$olw/2+$moh+$delta, $ch-$mb+$olw/2+$mov+$delta),
+	fill => 'transparent', stroke => $olc, strokewidth => $olw);
+#细内框
+$cimg->Draw(primitive => 'rectangle', points => get_points($ml-$delta, $mt-$delta, $cw-$mr+$delta, $ch-$mb+$delta),
+	fill => 'transparent', stroke => $ilc, strokewidth => $ilw);
 
-if($ftlw) { $cimg->Draw(primitive => 'line', points => get_points($cw/2, $mt-$mov, $cw/2, $fty-$flm), stroke => $flc, strokewidth => $ftlw); }
-if($fblw) { $cimg->Draw(primitive => 'line', points => get_points($cw/2, $fby+$flm, $cw/2, $ch-$mb+$mov), stroke => $flc, strokewidth => $fblw); }
-if($lgt) {
+#多栏模式时打印分栏横线
+if($ifmr and $mrn > 1) {
+	my $mrh = ($ch-$mt-$mb)/$mrn;
+	foreach my $rid (1..$mrn-1) {
+		$cimg->Draw(primitive => 'line', points => get_points($ml, $mt+$rid*$mrh, $cw/2-$lcw/2, $mt+$rid*$mrh), fill => $ilc);
+		$cimg->Draw(primitive => 'line', points => get_points($cw-$mr, $mt+$rid*$mrh, $cw/2+$lcw/2, $mt+$rid*$mrh), fill => $ilc);
+	}
+}
+#列细线
+foreach my $cid (1..$cln) {
+	my $tilc = ($ifmr and $mrn > 1) ? $mcc : $ilc; #多栏模式时，更新列细线颜色
+	$tilc = $ilc if($cid == $cln/2 or $cid == $cln/2+1); #版心两侧列细线
+	my $wd = ($cid > $cln/2) ? ($lcw-$clw) : 0; #对于越过版心列的横坐标调整
+	$cimg->Draw(primitive => 'line', points => get_points($ml+$wd+$clw*$cid, $mt, $ml+$wd+$clw*$cid, $ch-$mb),
+		fill => $tilc, stroke => $tilc, strokewidth => $ilw);
+}
+
+#上鱼尾
+draw_fishtop($fty, $ftrh, $ftth);
+#下鱼尾
+if($fbd == 0) { draw_fishbtm_down($fby, $fbrh, $fbth); } #顺鱼尾
+if($fbd == 1) { draw_fishbtm_up($fby, $fbrh, $fbth); } #对鱼尾
+#版心鱼尾到上下边框的粗线
+if($ftlw) { $cimg->Draw(primitive => 'line', points => get_points($cw/2, $mt-$mov-$delta, $cw/2, $fty-$flm), stroke => $flc, strokewidth => $ftlw); }
+if($fblw) { $cimg->Draw(primitive => 'line', points => get_points($cw/2, $fby+$flm, $cw/2, $ch-$mb+$mov+$delta), stroke => $flc, strokewidth => $fblw); }
+#版心底部的logo
+if(-f $lgi) { #图
+	my $logo = Image::Magick->new();
+	my ($lw, $lh);
+
+	$logo->ReadImage($lgi);
+	($lw, $lh) = ($logo->Get('Width'), $logo->Get('Height'));
+	$logo->AdaptiveResize(width => $lw/3, height => $lh/3, method => 'Hermit');
+	$cimg->Composite(image => $logo, x => $cw/2+$lcw/4-$lw/3/2, y => $ch-$mb-$lh/3, compose => 'Over');
+} elsif($lgt) { #文字
 	my @lchars = split //, $lgt;
 	foreach my $lcid (0..$#lchars) {
 		print "\t$lchars[$lcid] -> $lgf\n";
@@ -99,31 +139,34 @@ sub get_points {
 
 sub get_points_fish {
 	my ($x1, $y1, $x2, $y2, $x3, $y3, $x4, $y4, $x5, $y5) = @_;
-	return "M $x1,$y1 $x2,$y2 $x3,$y3 $x4,$y4 $x5,$y5 Z";
+	return "M $x1,$y1 $x2,$y2 $x3,$y3 $x4,$y4 $x5,$y5 Z"; #Draw path的路径参数格式，多点任意
 }
 
 sub get_points_path {
 	my ($fx, $fy, $px, $py, $tx, $ty) = @_;
-	return "M $fx,$fy $px,$py $tx,$ty Z";
+	return "M $fx,$fy $px,$py $tx,$ty Z"; #Draw path的路径参数格式，三角形
 }
 
+#上鱼尾
 sub draw_fishtop {
     my ($fy, $dy1, $dy2) = @_;
-    $cimg->Draw(primitive => 'line', points => get_points($cw/2-$lcw/2, $fy-$flm, $cw/2+$lcw/2, $fy-$flm),
-    	stroke => $flc, strokewidth => $flw);
+    #鱼尾上细线
+    $cimg->Draw(primitive => 'line', points => get_points($cw/2-$lcw/2, $fy-$flm, $cw/2+$lcw/2, $fy-$flm), stroke => $flc, strokewidth => $flw);
+    #鱼尾
     $cimg->Draw(
     	primitive => 'path',
     	points => get_points_fish($cw/2-$lcw/2, $fy, $cw/2+$lcw/2, $fy, $cw/2+$lcw/2, $fy+$dy1+$dy2, $cw/2, $fy+$dy1, $cw/2-$lcw/2, $fy+$dy1+$dy2),
     	stroke => $flc, strokewidth => $flw, fill => $ftc);
+    #鱼尾两细斜线
     $cimg->Draw(primitive => 'line', points => get_points($cw/2-$lcw/2, $fy+$dy1+$dy2+$flm, $cw/2, $fy+$dy1+$flm), fill => $flc);
     $cimg->Draw(primitive => 'line', points => get_points($cw/2, $fy+$dy1+$flm, $cw/2+$lcw/2, $fy+$dy1+$dy2+$flm), fill => $flc);
 }
-
+#下鱼尾，顺鱼尾
 sub draw_fishbtm_down {
     my ($fy, $dy1, $dy2) = @_;
     $cimg->Draw(primitive => 'line', points => get_points($cw/2-$lcw/2, $fy-$flm, $cw/2+$lcw/2, $fy-$flm),
     	stroke => $flc, strokewidth => $flw);
-    if($dy1 > 0 or $dy2 > 0) {
+    if($dy1 > 0 or $dy2 > 0) { #设置为0时，下鱼尾萎缩为双横线
 	    $cimg->Draw(
     		primitive => 'path',
     		points => get_points_fish($cw/2-$lcw/2, $fy, $cw/2+$lcw/2, $fy, $cw/2+$lcw/2, $fy+$dy1+$dy2, $cw/2, $fy+$dy1, $cw/2-$lcw/2, $fy+$dy1+$dy2),
@@ -133,12 +176,12 @@ sub draw_fishbtm_down {
 	$cimg->Draw(primitive => 'line', points => get_points($cw/2-$lcw/2, $fy+$dy1+$dy2+$flm, $cw/2, $fy+$dy1+$flm), fill => $flc);
     $cimg->Draw(primitive => 'line', points => get_points($cw/2, $fy+$dy1+$flm, $cw/2+$lcw/2, $fy+$dy1+$dy2+$flm), fill => $flc);
 }
-
+#下鱼尾，对鱼尾
 sub draw_fishbtm_up {
     my ($fy, $dy1, $dy2) = @_;
     $cimg->Draw(primitive => 'line', points => get_points($cw/2-$lcw/2, $fy+$flm, $cw/2+$lcw/2, $fy+$flm),
     	stroke => $flc, strokewidth => $flw);
-    if($dy1 > 0 or $dy2 > 0) {
+    if($dy1 > 0 or $dy2 > 0) { #设置为0时，下鱼尾萎缩为双横线
 	    $cimg->Draw(
     		primitive => 'path',
     		points => get_points_fish($cw/2-$lcw/2, $fy, $cw/2+$lcw/2, $fy, $cw/2+$lcw/2, $fy-$dy1-$dy2, $cw/2, $fy-$dy1, $cw/2-$lcw/2, $fy-$dy1-$dy2),
